01222991c9c4e3dd28fe39f8c55dbda6ca338d19
Pietroro98 <pietroro98@gmail.com>
2024-08-20 15:50:23 +0200

    aggiunta di un ancora che porta alla navbar

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 const MyFooter = function () {
 return(
-    <footer className="bg-dark text-white text-center py-3 fixed-bottom">
+    <footer className="bg-dark text-white text-center py-3 fixed-bottom ">
     <p>&copy; 2024 MyBookStore - Romano Pietro</p>
+    <a href="#top" className="text-white">
+        Back to top
+      </a>
   </footer>
 )
 }
--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -1,16 +1,17 @@
+import React from "react";
 import {Navbar, Container, Nav} from 'react-bootstrap'
 
 const MyNav = function () {
 return(
-    <Navbar collapseOnSelect expand="md" bg="dark" data-bs-theme="dark">
+    <Navbar id="top"collapseOnSelect expand="md" bg="dark" data-bs-theme="dark">
     <Container fluid>
       <Navbar.Brand href="#home">MyBookStore</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="me-auto">
-          <Nav.Link href="#">Home</Nav.Link>
-          <Nav.Link href="#">About</Nav.Link>
-          <Nav.Link href="#">browse</Nav.Link>
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#about">About</Nav.Link>
+          <Nav.Link href="#browse">browse</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Container>
