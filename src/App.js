e99988df4c8cd7d6ccff460575a5cab96d0faa09
Pietroro98 <pietroro98@gmail.com>
2024-08-21 15:19:00 +0200

    modifiche creazione singlebook

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import "bootstrap/dist/css/bootstrap.min.css"; //importo bootstrap per farlo fun
 import MyNav from "./components/MyNav";
 import MyFooter from "./components/MyFooter";
 import Welcome from "./components/Welcome";
-import AllTheBooks from "./components/AllTheBooks";
+// import AllTheBooks from "./components/AllTheBooks";
+import fantasyBooks from "../src/data/fantasy.json";
+import SingleBook from "./components/SingleBook";
+
 
 
 function App() {
   return (
-    <div className="App" style={{ backgroundImage: "url(https://www.dariocapozzi.it/wp-content/uploads/2022/09/background-image-css.jpg)" }}>
+    <div className="App vh-100" style={{ backgroundImage: "url(https://www.dariocapozzi.it/wp-content/uploads/2022/09/background-image-css.jpg)" }}>
       
       <header>
         <MyNav />
@@ -17,7 +20,8 @@ function App() {
 
       <main>
         <Welcome />
-        <AllTheBooks />
+        {/* <AllTheBooks /> */}
+        <SingleBook theBook={fantasyBooks[0]}/>
       </main>
 
       <footer>
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,18 +1,72 @@
-import { Card } from "react-bootstrap";
+import React, { Component } from "react";
+import { Card, Col, Container } from "react-bootstrap";
 
-const SingleBook = ({ book }) => (
-  <Card className="h-100">
-    <Card.Img variant="top" src={book.img} />
-    <Card.Body className="d-flex flex-column">
-      <Card.Title>{book.title}</Card.Title>
-      <Card.Text className="mt-auto">
-        <p>€{book.price}</p>
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted fw-bold">{book.category}</small>
-    </Card.Footer>
-  </Card>
-);
+// class SingleBook extends Component {
+//   state = {
+//     selected: false,
+//   };
+
+//   render() {
+//     return (
+//       <Container>
+//         <Col md={3} lg={2} key={this.props.theBook.asin}>
+//           <Card className="h-100">
+//             <Card.Img variant="top" src={this.props.theBook.img} />
+//             <Card.Body className="d-flex flex-column">
+//               <Card.Title>{this.props.theBook.title}</Card.Title>
+//               <Card.Text className="mt-auto">
+//                 <p>€{this.props.theBook.price}</p>
+//               </Card.Text>
+//             </Card.Body>
+//             <Card.Footer>
+//               <small className="text-muted fw-bold">{this.props.theBook.category}</small>
+//             </Card.Footer>
+//           </Card>
+//         </Col>
+//       </Container>
+//     );
+//   }
+// }
+
+// export default SingleBook;
+
+class SingleBook extends Component {
+  state = {
+    selected: false,
+  };
+
+  toggleSelected = () => {
+    this.setState((prevState) => ({
+      selected: !prevState.selected,
+    }));
+  };
+
+  render() {
+    const { theBook } = this.props;
+    const { selected } = this.state;
+
+    return (
+      <Container>
+        <Col md={3} lg={2} key={theBook.asin}>
+          <Card
+            className={`h-100 ${selected ? "border border-danger" : ""}`}  // Aggiunge un bordo rosso se selezionato
+            onClick={this.toggleSelected}  // Aggiunge il gestore di eventi per il click
+          >
+            <Card.Img variant="top" src={theBook.img} />
+            <Card.Body className="d-flex flex-column">
+              <Card.Title>{theBook.title}</Card.Title>
+              <Card.Text className="mt-auto">
+                <p>€{theBook.price}</p>
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted fw-bold">{theBook.category}</small>
+            </Card.Footer>
+          </Card>
+        </Col>
+      </Container>
+    );
+  }
+}
 
 export default SingleBook;
